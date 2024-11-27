357ab56ad1f553f39e9fad99199d234e885c3195
Mohammed reida <reidamohammed3@gmail.com>
2024-11-27 17:01:30 +0100

    ajouter la fonction addPlayer dans le fichier script.js

--- script.js
+++ script.js
@@ -1,32 +1,103 @@
 const btnAdd = document.getElementById("add")
+const bonTouch =document.querySelector(".bontouch")
+
+let plyr=0
+
+
+ function addPlayer(){
+    const player =
+    {
+      "name": document.getElementById("name").value,
+      "photo": document.getElementById("playerPhotoSelect").value,
+      "position": document.getElementById("Position").value,
+      "flag": document.getElementById("flagSelect").value,
+      "logo": document.getElementById("clubSelect").value,
+      "rating": document.getElementById("rating").value,
+      "pace": document.getElementById("pace").value,
+      "shooting": document.getElementById("shooting").value,
+      "passing": document.getElementById("passing").value,
+      "dribbling": document.getElementById("dribbling").value,
+      "defending": document.getElementById("defending").value,
+      "physical": document.getElementById("physical").value
+    }
     
+    return player;
+ }
 
-function displayImg(event) {
-    const img = document.getElementById("profileImage");
-    const text = document.getElementById("Text");
-    img.src = URL.createObjectURL(event.target.files[0]);
-    img.classList.remove("hidden");
-    text.classList.add("hidden");
+ function addHtml(player){
 
+    
+    bonTouch.innerHTML+=`
+    <div class="card chgm${plyr} p-10 relative grid place-items-center">
+       <div
+        class="flex text-center flex-col  absolute top-11 left-6"
+      >
+        <span class="font-bold text-[25px]">${player.rating}</span>
+        <span class="font-medium">${player.position}</span>
+      </div>
+      <img
+        src="${player.photo}"
+        alt=""
+        class="absolute top-12 left-8 "
+      /> 
+      <div
+        class="absolute bottom-16  font-semibold player-name-card "
+      >
+      ${player.name}
+      </div>
+      <div
+        class="flex flex-row gap-2 absolute bottom-10 left-5 font-semibold leading-3"
+      >
+        <div class="flex flex-col ">
+          <span class="text-[10px]">PAC</span>
+          <span class="text-[12px]">${player.pace}</span>
+        </div>
+        <div class="flex flex-col text-center">
+          <span class="text-[10px]">SHO</span>
+          <span class="text-[12px]">${player.shooting}</span>
+        </div>
+        <div class="flex flex-col ">
+          <span class="text-[10px]">PAS</span>
+          <span class="text-[12px]">${player.passing}</span>
+        </div>
+        <div class="flex flex-col ">
+          <span class="text-[10px]">DRI</span>
+          <span class="text-[12px]">${player.dribbling}</span>
+        </div>
+        <div class="flex flex-col ">
+          <span class="text-[10px]">DEF</span>
+          <span class="text-[12px]">${player.defending}</span>
+        </div>
+        <div class="flex flex-col ">
+          <span class="text-[10px]">PHY</span>
+          <span class="text-[12px]">${player.physical}</span>
+        </div>
+    </div>
+    <div
+        class=" flex flex-row justify-self-center gap-2 absolute bottom-4 left-16 player-name-card font-semibold "
+      > 
+      <img
+        src="${player.logo}"
+        alt=""
+        class="h-5 w-7 "
+      />
+      <img
+        src="${player.flag}"
+        alt=""
+        class="h-4 w-6 "
+      /> 
+        
+        </div>
+    </div>`
   }
 
-  
+ 
 btnAdd.addEventListener("click",()=>{
-const players =
-      {
-        "name": document.getElementById("name").value,
-        "photo": "https://cdn.sofifa.net/players/158/023/25_120.png",
-        "position": document.getElementById("Position").value,
-        "nationality": document.getElementById("nationality").value,
-        "flag": "https://cdn.sofifa.net/flags/ar.png",
-        "club": document.getElementById("club").value,
-        "logo": "https://cdn.sofifa.net/meta/team/239235/120.png",
-        "rating": document.getElementById("rating").value,
-        "pace": document.getElementById("pace").value,
-        "shooting": document.getElementById("shooting").value,
-        "passing": document.getElementById("passing").value,
-        "dribbling": document.getElementById("dribbling").value,
-        "defending": document.getElementById("defending").value,
-        "physical": document.getElementById("physical").value
-      }
-     })      
+    plyr++;
+  if(plyr>12){
+    alert("the dakka is full");
+    return
+  }else{
+   addHtml(addPlayer())
+}
+})      
